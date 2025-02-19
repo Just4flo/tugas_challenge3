1c40dd5517424a4bf613e4e3a46d6601f36846c1
Just4flo <144465104+Just4flo@users.noreply.github.com>
2025-02-19 19:14:45 +0700

    Update page.js

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,4 +1,6 @@
+
 import Link from "next/link";
+
 export default function Portfolio() {
   return (
     <section
@@ -24,6 +26,13 @@ export default function Portfolio() {
           Sistem ini membantu pengelolaan nilai mahasiswa secara efisien dalam institusi pendidikan.
         </p>
       </div>
+          <Link
+  href="/"
+  className="mt-6 px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300"
+>
+  Kembali ke Beranda
+</Link>
+
     </section>
   );
 }
