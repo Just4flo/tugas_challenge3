9871ee874f8d3f89957c26f1ca47646c4ecab9c6
Just4flo <144465104+Just4flo@users.noreply.github.com>
2025-02-19 19:14:08 +0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -18,10 +18,13 @@ export default function About() {
         adalah mahasiswa semester 4 di Universitas Masoem.
       </p>
 
-      {/* Link untuk kembali ke halaman utama */}
-      <Link href="/" className="mt-4 text-blue-400 underline">
-        Kembali ke Beranda
-      </Link>
+<Link
+  href="/"
+  className="mt-6 px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300"
+>
+  Kembali ke Beranda
+</Link>
+
     </section>
   );
 }
